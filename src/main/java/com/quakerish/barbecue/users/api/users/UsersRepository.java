5456653d08ad8eb66ea0f73e1ec11bc02e796bd4
iran <iranilsonhonorato88@gmail.com>
2026-05-04 00:01:00 -0300

    feat: implementando repository de users

--- src/main/java/com/quakerish/barbecue/users/api/users/UsersRepository.java
+++ src/main/java/com/quakerish/barbecue/users/api/users/UsersRepository.java
@@ -1,4 +1,15 @@
 package com.quakerish.barbecue.users.api.users;
 
-public interface UsersRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, String> {
+
+    Optional<Users> findByAuth0Id(String auth0Id);
+    Optional<Users> findByUsername(String username);
+    boolean existsByAuth0Id(String auth0Id);
+    boolean existsByUsername(String username);
 }
